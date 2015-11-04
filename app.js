122f4e197783a26a4430bf796182ab6e4761f72e
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2015-11-04 10:42:55 -0800

    Add title and upvotes properties to posts and iterate through them in the template

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', ['$scope', function($scope){
 	// scope serves as the bridge between Angular controllers and Angular templates. If you want something to be accessible in the template (function or variable), bind it to scope
 	$scope.posts = [
-	'post 1',
-	'post 2',
-	'post 3',
-	'post 4',
-	'post 5'
+	{title: 'Post 1', upvotes: 5},
+	{title: 'Post 2', upvotes: 4},
+	{title: 'Post 3', upvotes: 3},
+	{title: 'Post 4', upvotes: 2},
+	{title: 'Post 5', upvotes: 1}
 	]
 }]);
