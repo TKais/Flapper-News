ec3e857e5fa699bf4bccb119cde30e179ab6f970
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2015-11-04 10:32:31 -0800

    Make a list of posts using Angular

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', ['$scope', function($scope){
-	$scope.test = "Hello World";
+	// scope serves as the bridge between Angular controllers and Angular templates. If you want something to be accessible in the template (function or variable), bind it to scope
+	$scope.posts = [
+	'post 1',
+	'post 2',
+	'post 3',
+	'post 4',
+	'post 5'
+	]
 }]);
